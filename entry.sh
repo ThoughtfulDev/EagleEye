a29f5cceb976f2f9f27a94b773d04306e75119c6
ThoughtfulDev <thoughtful-dev@tuta.io>
2019-07-03 17:30:33 +0200

    Skip Yandex Search also in Docker by default (#70)

--- entry.sh
+++ entry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /EagleEye
-python3.6 eagle-eye.py --docker --name "Emeraude"
+python3.6 eagle-eye.py --skipyandex --docker --name "Emeraude"
 
 #now copy the result
 yes | cp -rf /EagleEye/*.pdf /result/
